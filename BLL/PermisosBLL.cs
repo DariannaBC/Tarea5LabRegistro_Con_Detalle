c9b7c20913d7408ba0e3485b6e753cc939277e8d
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2021-10-11 21:18:34 -0400

    Add project files.

---
+++ BLL/PermisosBLL.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea5LabRegistro_Con_Detalle.DAL;
+using Tarea5LabRegistro_Con_Detalle.Entidades;
+
+namespace Tarea5LabRegistro_Con_Detalle.BLL
+{
+    public class PermisosBLL
+    {
+       
+
+        public static Permisos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Permisos permisos;
+
+            try
+            {
+                permisos = contexto.Permisos.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return permisos;
+        }
+
+        
+
+        public static List<Permisos> GetList(Expression<Func<Permisos, bool>> criterio)
+        {
+            List<Permisos> lista = new List<Permisos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Permisos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+
+        public static List<Permisos> GetPermisos()
+        {
+            List<Permisos> lista = new List<Permisos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Permisos.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
